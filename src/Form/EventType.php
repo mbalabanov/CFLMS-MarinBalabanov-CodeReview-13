5e6d42af0888a62d8cfa6104d9821e28edd66413
Marin Balabanov <marincomics@gmail.com>
2020-12-04 22:38:18 +0200

    Working version with incomplete styling

---
+++ src/Form/EventType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('datetime')
+            ->add('description')
+            ->add('image')
+            ->add('capacity')
+            ->add('email')
+            ->add('phone')
+            ->add('address')
+            ->add('url')
+            ->add('type')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
