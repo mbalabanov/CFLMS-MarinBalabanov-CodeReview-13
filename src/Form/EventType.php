ca1781638eb68c26c26649bbd3bbdbebd3fc907d
Marin Balabanov <marincomics@gmail.com>
2020-12-05 17:07:41 +0200

    Adapted edit form with a select list

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -6,6 +6,7 @@ use App\Entity\Event;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class EventType extends AbstractType
 {
@@ -21,8 +22,14 @@ class EventType extends AbstractType
             ->add('phone')
             ->add('address')
             ->add('url')
-            ->add('type')
-        ;
+            ->add('type', ChoiceType::class, [
+                'choices'  => [
+                    'Sport' => 'Sport',
+                    'Music' => 'Music',
+                    'Opera' => 'Opera',
+                    'Ballet' => 'Ballet',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
